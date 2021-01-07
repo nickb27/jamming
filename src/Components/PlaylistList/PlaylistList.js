0f080e6884ed0e0eba951b3d093a2712e6c4f5c4
nickb27 <nick.brill@sportngin.com>
2021-01-07 08:24:36 -0600

    feature: add PlaylistList component files

---
+++ src/Components/PlaylistList/PlaylistList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './PlaylistList.css'
+
+class PlaylistList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      playlists: []
+    }
+  }
+
+  render() {
+    return(
+      <div className="PlaylistList">
+        
+      </div>
+    )
+  }
+}
+
+export default PlaylistList;
